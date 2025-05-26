4abd1d0e763611887e128ad8ea725bd77723b0ba
AgneBvdB <Agne.banzeryte@gmail.com>
2025-05-26 22:56:25 +0200

    26.05

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const countDate = new Date("Aug 9, 2025 00:01:00").getTime();
+const countDate = document.querySelector("#date")
 let counter = document.getElementById("counter");
-/* let btn = document.getElementById("user-btn"); */
+let btn = document.getElementById("date-btn"); 
 
-const intervalCounter = setInterval(function() {
-  let currentDate = new Date();
-  let distance = countDate - currentDate;
 
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 *60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+let intervalCounter;
+btn.addEventListener("click", () => {
+  clearInterval(intervalCounter);
+
+  const countDateValue = new Date(countDate.value);
+
+  intervalCounter = setInterval(function() {
+    const currentDate = new Date();
+    const distance = countDateValue - currentDate;
+
+    if(distance <= 0){
+      clearInterval(intervalCounter);
+      counter.innerText = "Finished!";
+      return;
+    } 
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 *60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-  counter.innerText = `${days} DIENOS ${hours} VALANDOS ${minutes} MINUTĖS`;
-
-/*   if(distance < 0){
-    clearInterval(intervalCounter);
-    counter.innerText = "Finished!";
-  } */
-}, 1000);
+    counter.innerText = `${days} Days ${hours} Hours`;
+  }, 1000);
+});
